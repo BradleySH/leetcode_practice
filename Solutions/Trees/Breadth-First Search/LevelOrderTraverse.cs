7d53b6a648d5af613fbac7a3ba1735dd1a6ac98d
BradleyHorlander <bradley.s.horlander@gmail.com>
2025-02-18 21:13:51 -0600

    Level order traversal BFS

--- Solutions/Trees/Breadth-First Search/LevelOrderTraverse.cs
+++ Solutions/Trees/Breadth-First Search/LevelOrderTraverse.cs
@@ -1,6 +1,45 @@
+using System.Collections;
+using Trees;
+
 namespace neetcodesolutions.Solutions.Trees.Breadth_First_Search;
 
 public class LevelOrderTraverse
 {
-    
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var result = new List<IList<int>>();
+        if (root is null)
+        {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            int levelSize = queue.Count;
+            var currLevel = new List<int>();
+            
+            // Dequeue levelSize nodes of the current level
+            for (int i = 0; i < levelSize; i++)
+            {
+                TreeNode node = queue.Dequeue();
+                currLevel.Add(node.Value);
+
+                if (node.Left != null)
+                {
+                    queue.Enqueue(node.Left);
+                }
+
+                if (node.Right != null)
+                {
+                    queue.Enqueue(node.Right);
+                }
+                
+            }
+            result.Add(currLevel);
+        }
+        return result;
+    }
 }
